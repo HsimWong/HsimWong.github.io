cc1e57e3059b1ce30a2e21bc8c7350c149e44753
Xin Wang <hsimwong1202@gmail.com>
2026-01-27 11:23:50 +0000

    Added script for installing kubectl

--- scripts/kubebuilder.sh
+++ scripts/kubebuilder.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+# Install kubectl
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+sudo tee >/etc/apt/sources.list.d/kubernetes.list <<EOF
+deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://mirrors.tuna.tsinghua.edu.cn/kubernetes/core:/stable:/v1.28/deb/ /
+deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://mirrors.tuna.tsinghua.edu.cn/kubernetes/addons:/cri-o:/stable:/v1.28/deb/ /
+EOF
+
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+
 # download kubebuilder and install locally.
-wget -O kubebuilder https://github.com/kubernetes-sigs/kubebuilder/releases/download/v4.11.0/kubebuilder_linux_amd64
+wget -O kubebuilder https://ghfast.top/https://github.com/kubernetes-sigs/kubebuilder/releases/download/v4.11.0/kubebuilder_linux_amd64
 chmod +x kubebuilder && sudo mv kubebuilder /usr/local/bin/
